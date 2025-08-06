448f7123ba814d822734cd0ec6702b1bdd7088cf
Danylo Diachenko <danylodiachenko@gmail.com>
2025-08-06 21:02:09 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { getImagesByQuery } from './js/pixabay-api';
 import iziToast from 'izitoast';
-// Додатковий імпорт стилів
+
 import 'izitoast/dist/css/iziToast.min.css';
 import { clearGallery, createGallery, showLoader } from './js/render-functions';
 
